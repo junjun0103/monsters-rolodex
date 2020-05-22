e7b2ff7827bb35850a77cc3a5cce10c6ebe5ed09
jun <jun@gmail.com>
2020-05-22 19:51:44 +1200

    Updates

---
+++ precache-manifest.a93efb5f7f6a87a0cccecb9ff3e1be20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "861fa207e1622c541ce3d7c3621e35a3",
+    "url": "/junjun0103/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bb99e23da63c84791c53",
+    "url": "/junjun0103/monsters-rolodex/static/css/main.dc911efe.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/junjun0103/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/junjun0103/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb99e23da63c84791c53",
+    "url": "/junjun0103/monsters-rolodex/static/js/main.4cff93b7.chunk.js"
+  },
+  {
+    "revision": "e6a34fcefc60ad1cd5a8",
+    "url": "/junjun0103/monsters-rolodex/static/js/runtime-main.5fd7a4e4.js"
+  }
+]);
